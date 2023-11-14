3070964e79210350b2ee48dedeaaf23ce507e5ae
Murilo Stein <murilo654456@gmail.com>
2023-11-14 20:46:06 -0300

    Comecando a implementar o tabuleiro

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using tabuleiro;
 
-namespace xadrez_console
-{
-    class Progream
-    {
-        static void Main(string[] args)
-        {
+namespace xadrez_console {
+    class Progream {
+        static void Main(string[] args) {
 
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8,8);
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
