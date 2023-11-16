99d427802167270286257f3d9a7168a9c74b2d41
Murilo Stein <murilo654456@gmail.com>
2023-11-16 20:26:34 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using tabuleiro;
 using Xadrez;
-using Xadrez_console;
 
 namespace xadrez_console {
-    class Progream {
+    class Program {
         static void Main(string[] args) {
 
             try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                while (!partida.terminada) {
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
--- Xadrez-console/Tela.cs
+++ Xadrez-console/Tela.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using tabuleiro;
+using Xadrez;
 
-namespace Xadrez_console {
+namespace xadrez_console {
     class Tela {
         
-        public static void imprimirTabuleiro(Tabuleiro tab) {
+            public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i<tab.linhas;  i++) {
                 Console.Write(8 - i + " ");
@@ -21,6 +22,13 @@ namespace Xadrez_console {
                     Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h ");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez() {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca) {
