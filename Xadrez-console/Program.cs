9310de42197fdb635d9d4897cc3dd2ff75488890
Murilo Stein <murilo654456@gmail.com>
2023-11-16 22:09:10 -0300

    Testes inicias com movimentos possiveis das peças

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -17,6 +17,14 @@ namespace xadrez_console {
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossives = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossives);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- Xadrez-console/tabuleiro/Peca.cs
+++ Xadrez-console/tabuleiro/Peca.cs
@@ -1,5 +1,5 @@
 ﻿namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
 
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -16,6 +16,8 @@
         public void incrementarQtdMovimentos() {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
 
     }
 }
